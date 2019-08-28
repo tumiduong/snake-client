ddb2f3ae67f12f01b24b801b37b302f5650d2fed
tumiduong <duong.julie@live.com>
2019-08-28 21:20:36 +0000

    completed snake game

--- input.js
+++ input.js
@@ -1,10 +1,23 @@
+let connection;
+
 const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
+  } else if (key === "\u0077") {
+    connection.write("Move: up");
+  } else if (key === "\u0065") {
+    connection.write("Move: down");
+  } else if (key === "\u0061") {
+    connection.write("Move: left");
+  } else if (key === "\u0066") {
+    connection.write("Move: right");
+  } else if (key === "\u0071") {
+    connection.write("Say: get away from me");
   }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
